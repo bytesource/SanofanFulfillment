d5ad037128488f88fb7497cd094205ea2a5cb040
bytesource <stefan.rohlfing@gmail.com>
2021-09-22 17:04:08 +0800

    Backup commit

--- src/Fulfillment/RawOrder.fs
+++ src/Fulfillment/RawOrder.fs
@@ -4,15 +4,24 @@ open System.IO
 open Thoth.Json.Net
 open Credentials
 
+open FsToolkit.ErrorHandling
+
+
+
 // TODO: 
 // 5) Consider separating the decoder code into another file, e.g. Order.Validation
 
 let getOrderJsonString = 
-    let file = "WoocommerceWebhookNewOrderResponseBody.json"
+    let file = "WooWebhookNewOrderResponseBody.json"
     let filePath = Path.Combine(Directory.GetCurrentDirectory(), "Playground", file) 
     printfn $"getOrderJson file path: {filePath}"
     File.ReadAllText filePath
 
+let getProductsJsonString = 
+    let file = "WooAllProducts.json"
+    let filePath = Path.Combine(Directory.GetCurrentDirectory(), "Playground", file) 
+    printfn $"getOrderJson file path: {filePath}"
+    File.ReadAllText filePath
 
 // ====================================================
 // Extract order data from the WooCommerce 
@@ -20,27 +29,27 @@ let getOrderJsonString =
 // ====================================================
 
 // NOTE: WooCommerce removes all leading and trailing whitespace.
-type WooPayment = {
+type RawPayment = {
     PaymentMethod: string
     TransactionId: string
 }
 with 
-    static member JsonDecoder : Decoder<WooPayment> = 
+    static member JsonDecoder : Decoder<RawPayment> = 
         Decode.object(fun get -> {
             PaymentMethod = get.Required.Field "payment_method" Decode.string
             TransactionId = get.Required.Field "transaction_id" Decode.string
         })
 
 
-type WooLineItemData = {
+type LineItemData = {
     ItemId: int
     Sku: string
     Name: string
     Price: float
 }
 with 
-    static member JsonDecoder : Decoder<WooLineItemData list> = 
-        let decodeOrderItem : Decoder<WooLineItemData> =
+    static member JsonDecoder : Decoder<LineItemData list> = 
+        let decodeOrderItem : Decoder<LineItemData> =
             Decode.object (fun get -> {
                 ItemId = get.Required.Field "id" Decode.int
                 Sku = get.Required.Field "sku" Decode.string
@@ -52,52 +61,138 @@ with
        
 
     static member FromJson json = 
-        Decode.fromString WooLineItemData.JsonDecoder json
+        Decode.fromString LineItemData.JsonDecoder json
 
 
-type WooProduct = {
+type RestProduct = {
     Id: int
-    Sku: string
+    Sku: string // Not always present
     Shippable: bool
 }
+with 
+    static member JsonDecoder : Decoder<RestProduct> = 
+        let decodeProduct : Decoder<RestProduct> =
+            Decode.object (fun get -> {
+                Id = get.Required.Field "id" Decode.int
+                Sku = get.Required.Field "sku" Decode.string
+                Shippable = get.Required.Field "shipping_required" Decode.bool
+            })
+
+        decodeProduct
+
+    static member FromJson json = 
+        Decode.fromString RestProduct.JsonDecoder json
 
 
-// let getWooJson credentials path = 
 
 
-TODO:
+// ===========================================
+// Test tube START
+// ===========================================
+// TODO:
 // Pack Woo credentials in record
 // Can I "bake-in credentials?"
-type GetWooApiProduct = 
-    Credenticals -> string -> Async<Result<WooProduct, string list>>
+// Bake the redentials in!!!!
+// type GetRestProduct = 
+//     Credentials.REST -> int -> Async<Result<RestProduct, string list>>
 
-let getWooApiProduct productId = 
-    let path = $"/wp-json/wc/v3/products/%i{productId}"
-    async{
+type GetRestProduct = 
+    int -> Async<Result<RestProduct, string list>>
+
+// Dummy implementation
+let getWooRestProduct (creds: Credentials.REST) : GetRestProduct = 
+    fun productId ->
+       async {
         return Ok {
             Id = productId
             Sku = "123"
             Shippable = true
+           }
+       }
+
+
+type SortedLineItem = 
+    | Shippable of LineItemData
+    | NonShippable of LineItemData
+
+
+module SortedLineItem = 
+
+    // Dummy implementation
+    let create : GetSortedLineItem =
+        fun getproduct lineItemData ->
+            async {
+                return Ok (Shippable lineItemData)
+            }
+
+
+    let create
+
+
+
+let getProduct = getWooRestProduct Credentials.Testing.wooRest
+
+
+let lineItemDataColl = 
+    LineItemData.FromJson getOrderJsonString
+    |> Result.mapError (fun e -> [ e ])
+
+
+let sortedLineItems = 
+    lineItemDataColl
+    |> Result.map (fun itemList -> 
+        itemList 
+        |> List.map (fun item -> 
+
+            item |> SortedLineItem.create getProduct
+        ))
+    |> Result.map (fun asyncResultSortedItems -> 
+        let result = 
+            asyncResultSortedItems
+            |> Async.Parallel
+            |> Async.RunSynchronously
+
+        result)
+    |> Result.map Array.toList
+
+
+
+module Async = 
+    
+    let map (f: 'a -> 'b) (aVal: Async<'a>) : Async<'b> = 
+        async {
+            let! result = aVal
+
+            return (f result)
         }
-    }
+
+    // RestProduct -> LineItemData -> SortedLineItem
+
+    // ((RestProduct -> SortedLineItem) -> C<RestProduct,_> -> C<SortedLineItem,_>)
+
+type GetRestProduct2 = 
+    int -> Async<Result<RestProduct, string list>>
+
+type GetSortedLineItem = LineItemData -> RestProduct -> SortedLineItem
+// Go fromAsync<Result<RestProduct>> to a Async<Result<SortedLineItem>>
+
+let sortedLineItems // Result<list<Async<Result<SortedLineItem list...
+    (getProduct: GetRestProduct2)
+    (getSortedLineItem: GetSortedLineItem) =
+
+    lineItemDataColl // Result<LineItemData list>
+    |> Result.map (List.map (fun item ->
+
+        asyncResult {
+            let! restProduct = getProduct item.ItemId
+            let sortedLineItem = getSortedLineItem item restProduct
+
+            return sortedLineItem
+        } ))
 
 
 
-type WooLineItem = 
-    | Shippable of WooLineItemData
-    | NonShippable of WooLineItemData
-
-module WooLineItem = 
-
-    let create (wooLineItemData: WooLineItemData) (wooProduct: WooProduct) = 
-        if wooProduct.Shippable
-            Shippable wooLineItemData
-        else
-            NonShippable wooLineItemData
-
-
-
-type WooAddress = {
+type RawAddress = {
     FirstName: string
     LastName: string
     Email: string
@@ -118,7 +213,7 @@ type WooAddress = {
 
 }
 with
-    static member JsonDecoder : Decoder<WooAddress>  = 
+    static member JsonDecoder : Decoder<RawAddress>  = 
         let addressDecoder = 
             Decode.object (fun get -> {
                 FirstName = get.Required.Field "first_name" Decode.string
@@ -141,31 +236,37 @@ with
 
 
 [<RequireQualifiedAccess>]
-type WooOrder = {
+type RawOrder = {
     OrderId: int
     Status: string
     Total: float
 
-    Payment: WooPayment
+    Payment: RawPayment
 
-    LineItems: WooLineItem list
+    LineItems: SortedLineItem list
 
-    ShipTo: WooAddress
+    ShipTo: RawAddress
 }
 with 
-    static member JsonDecoder : Decoder<WooOrder> = 
+    // TODO: need to add GetRestProduct dependency as well.
+    //Or just use the GetRestProduct dependency
+    static member JsonDecoder 
+        (sortedLineItems:  SortedLineItem list)
+        : Decoder<RawOrder> = 
+
         Decode.object(fun get -> {
             OrderId = get.Required.Field "id" Decode.int 
             Status = get.Required.Field "status" Decode.string 
             Total = get.Required.Field "total" (Decode.map float Decode.string)
 
-            Payment = get.Required.At [] WooPayment.JsonDecoder
+            Payment = get.Required.At [] RawPayment.JsonDecoder
 
-            LineItems = get.Required.At [] WooLineItem.JsonDecoder
+            // Returns Async<Result<SortedLineItem>
+            LineItems = sortedLineItems
 
-            ShipTo = get.Required.At [] WooAddress.JsonDecoder
+            ShipTo = get.Required.At [] RawAddress.JsonDecoder
         })
 
-    static member FromJson json = 
-        Decode.fromString WooOrder.JsonDecoder json
+    static member FromJson (sortedLineItems: SortedLineItem list) json = 
+        Decode.fromString (RawOrder.JsonDecoder sortedLineItems) json
 
